2465cb64c8683f3292ac0ec3d7adc0a853d5473d
yuechenxing <yuechenxing@zhaoshang.net>
2024-09-30 17:15:16 +0800

    测试

--- flood_fill/src/main/java/FloodFill.java
+++ flood_fill/src/main/java/FloodFill.java
@@ -5,5 +5,11 @@ import java.awt.*;
  */
 public interface FloodFill {
 
+    /**
+     * 查找point从横向和竖向有关联的所有点的坐标，左上角坐标从0，0开始
+     * @param points 点数据
+     * @param point 选中的坐标点
+     * @return 返回相关的坐标数组
+     */
     Point[] searchRectPoint(Integer[][] points, Point point);
 }
